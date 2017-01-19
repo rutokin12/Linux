f4f612300e3f37dd88466ed0855350f012fd8247
Billy <rutokin12@gmail.com>
2017-01-19 10:12:27 -0800

    my script

---
+++ installs.sh
@@ -0,0 +1,36 @@
+GET = apt install
+
+#git
+$GET git
+git config --global user.email "rutokin12@gmail.com"
+
+# Spotify
+#1 Add the Spotify repository signing key to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# 3. Update list of available packages
+sudo apt-get update
+
+# 4. Install Spotify
+sudo apt-get install spotify-client
+
+# mumble     
+$GET mumble
+
+#tor browser
+$GET torbrowser-launcher
+
+#meld
+$GET meld
+
+# vim
+$GET vim
+
+# vlc
+$GET vlc
+
+# tree
+$GET tree  
